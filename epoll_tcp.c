5ea1e42a77d634246e0e410f2e930a95e2f124d0
lawlite <849451478@qq.com>
2022-06-27 23:45:06 +0800

    epoll demo

--- epoll_tcp.c
+++ epoll_tcp.c
@@ -14,6 +14,7 @@
 int main(int argc, char ** argv) {
 	int serverFd, clientFd;
 	int len, ret, rlen;
+	char buf[128];
 	struct sockaddr_in serverAddr, clientAddr;
 
 	if (argc != 2) {
@@ -23,7 +24,7 @@ int main(int argc, char ** argv) {
 
 	short port;
 	port = atoi(argv[1]);
-	len = sizeof(serverAddr)
+	len = sizeof(serverAddr);
 	serverFd = socket(AF_INET, SOCK_STREAM, 0);
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(port);
@@ -46,7 +47,43 @@ int main(int argc, char ** argv) {
 	struct epoll_event event;
 	// 定义epoll事件集合
 	struct epoll_event events[20];
-	memset(events, 0, 20, *sizeof(struct epoll_event))
+	memset(events, 0, 20 * sizeof(struct epoll_event));
+	// 创建epoll的fd，红黑树
 	epfd = epoll_create(1);
 	
+	event.data.fd = serverFd;
+	// 填充事件类型，监听读事件
+	event.events = EPOLLIN;
+	// 把serverFd(监听FD)和事件添加到红黑树上
+	epoll_ctl(epfd, EPOLL_CTL_ADD, serverFd, &event);
+
+	while(1) {
+		// 等待时间到来，阻塞模式，同时监听20个事件，返回就绪的事件个数
+		epct = epoll_wait(epfd, events, 20, -1);
+		// 根据epoll返回的值查询事件
+		for (i = 0; i < epct; i++) {
+				if (events[i].data.fd == serverFd) {
+					socklen_t length = sizeof(clientAddr);
+					clientFd = accept(events[i].data.fd, (struct sockaddr *)&clientAddr, &length);
+					printf("new fd=%d ip %s\n", clientFd, inet_ntoa(clientAddr.sin_addr));
+					event.data.fd = clientFd;
+					event.events = EPOLLIN | EPOLLET;
+					// event.events = EPOLLIN;
+					epoll_ctl(epfd, EPOLL_CTL_ADD, clientFd, &event);
+				} else {
+					printf("new data arrive\n");
+					// 如果不是serverFd, 就是clientFd的读事件
+					memset(buf, 0, BUFLEN);
+					rlen = read(events[i].data.fd, buf, BUFLEN);
+					if (rlen <= 0) {
+						// 客户端断开
+						printf("fd %d disconnected\n", events[i].data.fd);
+						close(events[i].data.fd);
+						epoll_ctl(epfd, EPOLL_CTL_DEL, events[i].data.fd, &event);
+						continue;
+					}
+					printf("fd: %d data: %s\n", events[i].data.fd, buf);
+				}
+		}
+	}
 }
